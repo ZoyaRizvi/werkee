5f33a8e49152ac1f0c40956bceaa21c17f9c8c3c
ZoyaRizvi <81320848+ZoyaRizvi@users.noreply.github.com>
2024-05-13 14:01:49 +0500

    fixed login issue

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,20 @@ import { Routes, Route, Navigate, useRoutes } from "react-router-dom";
 import { Dashboard, Auth } from "@/layouts";
 import Landing from './layouts/landing/Landing.jsx';
 import { AuthProvider } from "./context/authContext/index.jsx"
+import {
+  HomeIcon,
+  UserCircleIcon,
+  TableCellsIcon,
+  InformationCircleIcon,
+  ServerStackIcon,
+  RectangleStackIcon,
+} from "@heroicons/react/24/solid";
+import { Home, Profile, Tables, Notifications } from "@/pages/dashboard";
+import { SignIn, SignUp } from "@/pages/auth";
+
+const icon = {
+  className: "w-5 h-5 text-inherit",
+};
 
 function App() {
   // <Route path='/' element={<Landing/>}></Route>
@@ -20,7 +34,19 @@ function App() {
     {
       path: "/dashboard/*",
       element: <Dashboard />,
-    }
+    },
+    // {
+    //   icon: <ServerStackIcon {...icon} />,
+    //   name: "sign in",
+    //   path: "/sign-in",
+    //   element: <SignIn />,
+    // },
+    // {
+    //   icon: <RectangleStackIcon {...icon} />,
+    //   name: "sign up",
+    //   path: "/sign-up",
+    //   element: <SignUp />,
+    // },
   ]
   let routesElement= useRoutes(arr)
   return (
