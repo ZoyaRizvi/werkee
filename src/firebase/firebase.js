87adadcd5acfa968e46a1340d6fe4ecf74457374
ZoyaRizvi <81320848+ZoyaRizvi@users.noreply.github.com>
2024-07-27 14:34:58 +0500

    Revert "updating-data-to-collection"

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,27 +1,21 @@
-// Import the necessary Firebase modules
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-import { getStorage } from "firebase/storage";
 
-// Your web app's Firebase configuration
+
 const firebaseConfig = {
-  apiKey: "AIzaSyCfy0sIm6sVZh1dQN86Ppyrlv9HhTa2wI0",
-  authDomain: "werky-a2729.firebaseapp.com",
-  projectId: "werky-a2729",
-  storageBucket: "werky-a2729.appspot.com",
-  messagingSenderId: "449488700866",
-  appId: "1:449488700866:web:893b9fe0f02fd5cff3425b",
-  measurementId: "G-5SH8NHPRE1"
-};
+    apiKey: "AIzaSyCfy0sIm6sVZh1dQN86Ppyrlv9HhTa2wI0",
+    authDomain: "werky-a2729.firebaseapp.com",
+    projectId: "werky-a2729",
+    storageBucket: "werky-a2729.appspot.com",
+    messagingSenderId: "449488700866",
+    appId: "1:449488700866:web:893b9fe0f02fd5cff3425b",
+    measurementId: "G-5SH8NHPRE1"
+  };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const auth = getAuth(app)
+const db = getFirestore(app)
 
-// Get a reference to the auth, firestore, and storage services
-const auth = getAuth(app);
-const db = getFirestore(app);
-const storage = getStorage(app);
 
-// Export the Firebase services
-export { app, auth, db, storage };
+export { app, auth, db};
