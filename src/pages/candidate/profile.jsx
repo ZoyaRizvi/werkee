05d4010621dd44f495e9a4195b27d3bfc37b296b
ZoyaRizvi <81320848+ZoyaRizvi@users.noreply.github.com>
2024-07-31 16:29:40 +0500

    Update profile.jsx

--- src/pages/candidate/profile.jsx
+++ src/pages/candidate/profile.jsx
@@ -37,7 +37,7 @@ import { useNavigate } from 'react-router-dom';
 import { db, storage } from "@/firebase/firebase";
 import { doc, updateDoc, getDoc } from "firebase/firestore";
 import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
-import Projects from './projects';
+import Projects from './Projects';
 
 // Default values
 const defaultProfile = {
