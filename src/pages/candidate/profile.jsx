6dd702c4a63082268c24c3c66048dd89b00602e9
ZoyaRizvi <81320848+ZoyaRizvi@users.noreply.github.com>
2024-08-14 17:19:10 +0500

    Update profile.jsx

--- src/pages/candidate/profile.jsx
+++ src/pages/candidate/profile.jsx
@@ -37,7 +37,7 @@ export function Profile() {
   const [badges, setBadges] = useState([]);
 
   const handleStartTest = (skill) => {
-    navigate('/skillassessment', { state: { skill } });
+    navigate('/dashboard/skillassessment', { state: { skill } });
   };
   
   useEffect(() => {
@@ -237,12 +237,12 @@ export function Profile() {
                 ),
                 badge:(
                   <div className="badge-container">
-                  {badges.length > 0 && Array.isArray(badges) && badges.map((badge, index) => (
-                  <Tooltip key={index} content={badge} placement="top">
-                    <TrophyIcon className="h-6 w-6 text-orange-500" />
-                  </Tooltip>
+                  {badges.length > 0 && badges.map((badge, index) => (
+                    <Tooltip key={index} content={badge} placement="top">
+                      <TrophyIcon className="h-6 w-6 text-orange-500" />
+                    </Tooltip>
                   ))}
-                  </div>
+                </div>
                 )
               }}
               action={
