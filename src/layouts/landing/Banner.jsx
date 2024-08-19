439d8f0a951ec4dd285b35fa751599a3c0d476b0
Maheen Khalid <83791215+Maheen-khalid@users.noreply.github.com>
2024-08-19 21:19:31 +0500

    Responsive Navbar

--- src/layouts/landing/Banner.jsx
+++ src/layouts/landing/Banner.jsx
@@ -6,51 +6,51 @@ export default function Banner() {
     <div className="antialiased">
       <div className="bg-[#FFF2E1]">
         <div className="max-w-screen-xl px-8 mx-auto flex flex-col lg:flex-row items-start">
-          <div className="flex flex-col w-full lg:w-6/12 justify-center lg:pt-24 items-start text-center lg:text-left mb-5 md:mb-0">
-            <h1 data-aos="fade-right" data-aos-once="true" className="my-4 text-5xl font-bold leading-tight text-darken">
+          <div className="flex flex-col w-full lg:w-6/12 justify-center lg:pt-24 items-center lg:items-start text-center lg:text-left mb-5 md:mb-0">
+            <h1 data-aos="fade-right" data-aos-once="true" className="my-4 text-3xl md:text-5xl font-bold leading-tight text-darken">
               <span className="text-teal-500">Earn</span> your
               first income
             </h1>
-            <p data-aos="fade-down" data-aos-once="true" data-aos-delay="300" className="leading-normal text-2xl mb-8">
+            <p data-aos="fade-down" data-aos-once="true" data-aos-delay="300" className="leading-normal text-lg md:text-2xl mb-8">
               Real companies, real projects, real earnings and real money
               <br/><br/> Smart opportunities for the smart generation
             </p>
-            <div data-aos="fade-up" data-aos-once="true" data-aos-delay="700" className="w-full md:flex items-center justify-center lg:justify-start md:space-x-5">
+            <div data-aos="fade-up" data-aos-once="true" data-aos-delay="700" className="w-full flex flex-col md:flex-row items-center justify-center lg:justify-start space-y-4 md:space-y-0 md:space-x-5">
               <Link to="/auth/sign-up">
-                <button className="lg:mx-0 bg-teal-500 text-white text-xl font-bold rounded-full py-4 px-9 focus:outline-none transform transition hover:scale-110 duration-300 ease-in-out">
+                <button className="bg-teal-500 text-white text-lg md:text-xl font-bold rounded-full py-3 md:py-4 px-6 md:px-9 focus:outline-none transform transition hover:scale-110 duration-300 ease-in-out">
                   Get Started
                 </button>
               </Link>
-              <div className="flex items-center justify-center space-x-3 mt-5 md:mt-0 focus:outline-none transform transition hover:scale-110 duration-300 ease-in-out">
+              <div className="flex items-center justify-center space-x-3 mt-5 md:mt-0">
                 <Link to="/how-it-works">
-                  <button className="bg-white w-14 h-14 rounded-full flex items-center justify-center">
-                    <svg className="w-5 h-5 ml-2" viewBox="0 0 24 28" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <button className="bg-white w-12 h-12 md:w-14 md:h-14 rounded-full flex items-center justify-center">
+                    <svg className="w-4 h-4 md:w-5 md:h-5" viewBox="0 0 24 28" fill="none" xmlns="http://www.w3.org/2000/svg">
                       <path d="M22.5751 12.8097C23.2212 13.1983 23.2212 14.135 22.5751 14.5236L1.51538 27.1891C0.848878 27.5899 5.91205e-07 27.1099 6.25202e-07 26.3321L1.73245e-06 1.00123C1.76645e-06 0.223477 0.848877 -0.256572 1.51538 0.14427L22.5751 12.8097Z" fill="#23BDEE"/>
                     </svg>
                   </button>
                 </Link>
-                <span className="cursor-pointer">Watch how it works</span>
+                <span className="cursor-pointer text-sm md:text-base">Watch how it works</span>
               </div>
             </div>
           </div>
           
-          <div className="w-full lg:w-6/12 lg:-mt-10 relative" id="girl">
-            <img data-aos="fade-up" data-aos-once="true" className="w-10/10 mx-auto 2xl:-mb-20" src="/img/girl.png" />
+          <div className="w-full lg:w-6/12 relative mt-8 lg:mt-0" id="girl">
+            <img data-aos="fade-up" data-aos-once="true" className="w-full h-auto mx-auto" src="/img/girl.png" alt="Girl"/>
             
-            <div data-aos="fade-up" data-aos-delay="300" data-aos-once="true" className="absolute top-16 -left-6 sm:top-28 sm:left-20 md:top-36 md:left-20 lg:left-4 lg:top-48 floating-4">
-               <img height={'400px'} className="bg-opacity-80 rounded-lg h-50 sm:h-16" src='/img/bot.png'/>
+            <div data-aos="fade-up" data-aos-delay="300" data-aos-once="true" className="absolute top-16 -left-6 sm:top-24 sm:left-12 md:top-32 md:left-12 lg:left-4 lg:top-48 floating-4">
+               <img height={'400px'} className="bg-opacity-80 rounded-lg h-32 sm:h-24" src='/img/bot.png' alt="Bot"/>
             </div>
           
-            <div data-aos="fade-up" data-aos-delay="400" data-aos-once="true" className="absolute top-20 right-10 sm:right-24 sm:top-28 md:top-36 md:right-32 lg:top-32 lg:right-16 floating">
-              <img className="h-16 sm:h-24" viewBox="0 0 149 149" src='/img/like.png'></img>
+            <div data-aos="fade-up" data-aos-delay="400" data-aos-once="true" className="absolute top-20 right-10 sm:right-16 sm:top-24 md:top-32 md:right-24 lg:top-32 lg:right-8 floating">
+              <img className="h-12 sm:h-16 md:h-20" src='/img/like.png' alt="Like"/>
             </div>
 
-            <div data-aos="fade-up" data-aos-delay="500" data-aos-once="true" className="absolute bottom-14 -left-4 sm:left-2 sm:bottom-20 lg:bottom-24 lg:-left-4 floating">
-              <img className=" bg-opacity-80 rounded-lg h-20 sm:h-28" src="/img/cat-laptop.png" alt=""/>
+            <div data-aos="fade-up" data-aos-delay="500" data-aos-once="true" className="absolute bottom-14 sm:left-4 sm:bottom-16 lg:bottom-24 lg:left-0 floating">
+              <img className="bg-opacity-80 rounded-lg h-20 sm:h-24" src="/img/cat-laptop.png" alt="Cat Laptop"/>
             </div>
             
-            <div data-aos="fade-up" data-aos-delay="600" data-aos-once="true" className="absolute bottom-20 md:bottom-48 lg:bottom-52 -right-6 lg:right-8 floating-4">
-              <img className=" bg-opacity-80 rounded-lg h-18 sm:h-16" src="/img/bot2.png" alt=""/>
+            <div data-aos="fade-up" data-aos-delay="600" data-aos-once="true" className="absolute bottom-20 md:bottom-32 lg:bottom-40 right-4 lg:right-8 floating-4">
+              <img className="bg-opacity-80 rounded-lg h-16 sm:h-20" src="/img/bot2.png" alt="Bot 2"/>
             </div>
           </div>
         </div>
@@ -62,5 +62,5 @@ export default function Banner() {
         </div>
       </div>
     </div>
-  )
+  );
 }
--- src/layouts/landing/Nav.jsx
+++ src/layouts/landing/Nav.jsx
@@ -6,24 +6,71 @@ function Navbar() {
     return (
         <div className="w-full text-gray-700 bg-[#FFF2E1]">
             <div className="flex flex-col max-w-screen-xl px-8 mx-auto md:items-center md:justify-between md:flex-row">
-                <div className="flex flex-row items-center justify-between">
-                    <div className="relative md:mt-8">
-                        <a href="#" className="text-lg relative z-50 font-bold tracking-widest text-gray-900 rounded-lg focus:outline-none focus:shadow-outline"><img width={"200px"} height={"100px"} src='./img/logo.png'></img></a>
+                <div className="flex flex-row items-center justify-between py-4 md:py-8">
+                    <div className="relative">
+                        <a href="#" className="text-lg font-bold tracking-widest text-gray-900 rounded-lg focus:outline-none focus:shadow-outline">
+                            <img width="200" height="100" src='./img/logo.png' alt="Logo" />
+                        </a>
                     </div>
-                    <button className="rounded-lg md:hidden focus:outline-none focus:shadow-outline" onClick={() => setOpen(!open)}>
+                    <button
+                        className="rounded-lg md:hidden focus:outline-none focus:shadow-outline"
+                        onClick={() => setOpen(!open)}
+                    >
                         <svg fill="currentColor" viewBox="0 0 20 20" className="w-6 h-6">
-                            <path style={{ display: !open ? 'block' : 'none' }} fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM9 15a1 1 0 011-1h6a1 1 0 110 2h-6a1 1 0 01-1-1z" clipRule="evenodd"></path>
-                            <path style={{ display: open ? 'block' : 'none' }} fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd"></path>
+                            <path
+                                style={{ display: !open ? 'block' : 'none' }}
+                                fillRule="evenodd"
+                                d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM9 15a1 1 0 011-1h6a1 1 0 110 2h-6a1 1 0 01-1-1z"
+                                clipRule="evenodd"
+                            ></path>
+                            <path
+                                style={{ display: open ? 'block' : 'none' }}
+                                fillRule="evenodd"
+                                d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                                clipRule="evenodd"
+                            ></path>
                         </svg>
                     </button>
                 </div>
-                <nav className={`h-0 md:h-auto flex flex-col flex-grow md:items-center pb-4 md:pb-0 md:flex md:justify-end md:flex-row origin-top duration-300 ${open ? 'scale-y-0' : 'transform md:transform-none'}`}>
-                    <a className="px-4 py-2 mt-2 text-sm bg-transparent rounded-lg md:mt-8 md:ml-4 hover:text-gray-900 focus:outline-none focus:shadow-outline" href="#">Home</a>
-                    <a className="px-4 py-2 mt-2 text-sm bg-transparent rounded-lg md:mt-8 md:ml-4 hover:text-gray-900 focus:outline-none focus:shadow-outline" href="#about">About Us</a>
-                    <a className="px-4 py-2 mt-2 text-sm bg-transparent rounded-lg md:mt-8 md:ml-4 hover:text-gray-900 focus:outline-none focus:shadow-outline" href="#whyteens">Why Teens</a>
-                    <a className="px-4 py-2 mt-2 text-sm bg-transparent rounded-lg md:mt-8 md:ml-4 hover:text-gray-900 focus:outline-none focus:shadow-outline" href="#whatsnew">What's new</a>
-                    <a className="px-10 py-3 mt-2 text-sm text-center bg-white text-gray-800 rounded-full md:mt-8 md:ml-4" href="/auth/sign-in">Login</a>
-                    <a className="px-10 py-3 mt-2 text-sm text-center bg-teal-500 text-white rounded-full md:mt-8 md:ml-4" href="/auth/sign-up">Sign Up</a>
+                <nav
+                    className={`md:flex md:items-center md:justify-end md:flex-row flex flex-col pb-4 md:pb-0 md:scale-100 transform duration-300 ${open ? 'scale-100' : 'scale-y-0'}`}
+                >
+                    <a
+                        className="px-4 py-2 text-sm bg-transparent rounded-lg md:mt-0 mt-2 md:ml-4 hover:text-gray-900 focus:outline-none focus:shadow-outline"
+                        href="#"
+                    >
+                        Home
+                    </a>
+                    <a
+                        className="px-4 py-2 text-sm bg-transparent rounded-lg md:mt-0 mt-2 md:ml-4 hover:text-gray-900 focus:outline-none focus:shadow-outline"
+                        href="#about"
+                    >
+                        About Us
+                    </a>
+                    <a
+                        className="px-4 py-2 text-sm bg-transparent rounded-lg md:mt-0 mt-2 md:ml-4 hover:text-gray-900 focus:outline-none focus:shadow-outline"
+                        href="#whyteens"
+                    >
+                        Why Teens
+                    </a>
+                    <a
+                        className="px-4 py-2 text-sm bg-transparent rounded-lg md:mt-0 mt-2 md:ml-4 hover:text-gray-900 focus:outline-none focus:shadow-outline"
+                        href="#whatsnew"
+                    >
+                        What's new
+                    </a>
+                    <a
+                        className="px-10 py-3 text-sm text-center bg-white text-gray-800 rounded-full md:mt-0 mt-2 md:ml-4"
+                        href="/auth/sign-in"
+                    >
+                        Login
+                    </a>
+                    <a
+                        className="px-10 py-3 text-sm text-center bg-teal-500 text-white rounded-full md:mt-0 mt-2 md:ml-4"
+                        href="/auth/sign-up"
+                    >
+                        Sign Up
+                    </a>
                 </nav>
             </div>
         </div>
@@ -31,3 +78,4 @@ function Navbar() {
 }
 
 export default Navbar;
+
