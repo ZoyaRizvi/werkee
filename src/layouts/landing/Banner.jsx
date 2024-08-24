046b067514a5032340813cc828bd456ed324146a
ZoyaRizvi <81320848+ZoyaRizvi@users.noreply.github.com>
2024-08-24 13:01:15 +0500

    Fixed UI

--- src/layouts/landing/Banner.jsx
+++ src/layouts/landing/Banner.jsx
@@ -12,9 +12,9 @@ export default function Banner() {
               first income
             </h1>
             <p data-aos="fade-down" data-aos-once="true" data-aos-delay="300" className="leading-normal text-lg md:text-2xl mb-8">
-              Real companies, real projects, real earnings and real money
-              <br/><br/> Smart opportunities for the smart generation
+            Real projects, real earnings—smart opportunities for the smart generation
             </p>
+            <br></br>
             <div data-aos="fade-up" data-aos-once="true" data-aos-delay="700" className="w-full flex flex-col md:flex-row items-center justify-center lg:justify-start space-y-4 md:space-y-0 md:space-x-5">
               <Link to="/auth/sign-up">
                 <button className="bg-teal-500 text-white text-lg md:text-xl font-bold rounded-full py-3 md:py-4 px-6 md:px-9 focus:outline-none transform transition hover:scale-110 duration-300 ease-in-out">
@@ -34,24 +34,26 @@ export default function Banner() {
             </div>
           </div>
           
-          <div className="w-full lg:w-6/12 relative mt-8 lg:mt-0" id="girl">
+          <div className="w-full lg:w-4/12 relative mt-8 lg:mt-0" id="girl">
             <img data-aos="fade-up" data-aos-once="true" className="w-full h-auto mx-auto" src="/img/girl.png" alt="Girl"/>
             
-            <div data-aos="fade-up" data-aos-delay="300" data-aos-once="true" className="absolute top-16 -left-6 sm:top-24 sm:left-12 md:top-32 md:left-12 lg:left-4 lg:top-48 floating-4">
-               <img height={'400px'} className="bg-opacity-80 rounded-lg h-32 sm:h-24" src='/img/bot.png' alt="Bot"/>
+            <div data-aos="fade-up" data-aos-delay="300" data-aos-once="true" className="absolute top-16 -left-16 sm:top-24 sm:-left-12 md:top-32 md:-left-16 lg:-left-8 lg:top-48 floating-4">
+                <img height={'400px'} className="bg-opacity-80 rounded-lg h-32 sm:h-24" src='/img/bot.png' alt="Bot"/>
             </div>
+
           
-            <div data-aos="fade-up" data-aos-delay="400" data-aos-once="true" className="absolute top-20 right-10 sm:right-16 sm:top-24 md:top-32 md:right-24 lg:top-32 lg:right-8 floating">
+            <div data-aos="fade-up" data-aos-delay="400" data-aos-once="true" className="absolute top-20 right-10 sm:right-16 sm:top-24 md:top-32 md:right-24 lg:top-3 lg:right-1 floating">
               <img className="h-12 sm:h-16 md:h-20" src='/img/like.png' alt="Like"/>
             </div>
 
-            <div data-aos="fade-up" data-aos-delay="500" data-aos-once="true" className="absolute bottom-14 sm:left-4 sm:bottom-16 lg:bottom-24 lg:left-0 floating">
-              <img className="bg-opacity-80 rounded-lg h-20 sm:h-24" src="/img/cat-laptop.png" alt="Cat Laptop"/>
+            <div data-aos="fade-up" data-aos-delay="500" data-aos-once="true" className="absolute bottom-14 sm:-left-6 sm:bottom-16 lg:bottom-24 lg:-left-16 floating">
+                <img className="bg-opacity-80 rounded-lg h-20 sm:h-24" src="/img/cat-laptop.png" alt="Cat Laptop"/>
             </div>
             
-            <div data-aos="fade-up" data-aos-delay="600" data-aos-once="true" className="absolute bottom-20 md:bottom-32 lg:bottom-40 right-4 lg:right-8 floating-4">
-              <img className="bg-opacity-80 rounded-lg h-16 sm:h-20" src="/img/bot2.png" alt="Bot 2"/>
-            </div>
+            <div data-aos="fade-up" data-aos-delay="600" data-aos-once="true" className="absolute bottom-20 md:bottom-32 lg:bottom-38 lg:-right-12 floating-4">
+  <img className="bg-opacity-80 rounded-lg h-16 sm:h-20" src="/img/bot2.png" alt="Bot 2"/>
+</div>
+
           </div>
         </div>
         <div className="text-white -mt-14 sm:-mt-24 lg:-mt-36 z-40 relative">
